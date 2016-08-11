a062b827490a16fc7d32be4b222f27a104617800
radai-rosenblatt <radai.rosenblatt@gmail.com>
2016-08-10 18:56:57 -0700

    more work

---
+++ avro17using14gen/src/test/java/net/radai/avroplayground/avro17using14gen/ClientCodeTest.java
@@ -0,0 +1,13 @@
+package net.radai.avroplayground.avro17using14gen;
+
+import net.radai.avroplayground.Pojo;
+import org.junit.Test;
+
+
+public class ClientCodeTest {
+
+  @Test
+  public void testInteroperability() throws Exception {
+    Pojo pojo = new Pojo();
+  }
+}
